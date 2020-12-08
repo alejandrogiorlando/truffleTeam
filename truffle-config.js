65ce1928f59f47c65707a77e9536b47c428a98d2
Alejandro Giorlando <alejandrogiorlando@gmail.com>
2020-12-08 13:43:25 -0300

    adding coin SmartContract

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const teamsMnemonic = "seminar lady sad rubber empower spider head chase multiply barrel emerge large";
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -36,6 +38,7 @@ module.exports = {
    */
 
   networks: {
+    
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
@@ -47,6 +50,12 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+    teams: {
+      provider: function() {
+        return new HDWalletProvider(teamsMnemonic, "https://sandbox.truffleteams.com/db7c1f7d-51e3-4fcb-bafd-4219573f126e", 0, 10, false);
+      },
+      network_id: 1,
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -82,7 +91,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+       version: "0.6.12",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
